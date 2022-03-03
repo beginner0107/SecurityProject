74cde22b5f5b92dc5b3a66b17e979a329abd4fed
Beginner <dkstmdwn17@naver.com>
2022-03-03 17:09:26 +0900

    매개변수 하나가 빠져있었다.
    replyer 추가

--- src/main/webapp/resources/js/reply.js
+++ src/main/webapp/resources/js/reply.js
@@ -39,7 +39,7 @@ var replyService = (function(){
 			});
 	}
 	
-	function remove(rno, callback, error){
+	function remove(rno, replyer, callback, error){
 		$.ajax({
 		  type : 'delete',
 		  url : '/replies/' + rno,
